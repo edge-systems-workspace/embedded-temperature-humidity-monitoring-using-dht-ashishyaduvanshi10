d614a074d95571797920655ed35628f43c35389e
ashish.yadav_cs.aiml24@gla.ac.in <ashishkumar12333ak@gmail.com>
2026-02-20 02:29:44 +0530

    Initialize serial and DHT sensor

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+void setup(){
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
 }
 
 void loop() {
 }
+
 
 
