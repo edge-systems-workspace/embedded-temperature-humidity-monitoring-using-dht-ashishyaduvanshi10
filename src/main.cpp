b7423b9898e339209ecda759268ef148d89fe9d0
ashish.yadav_cs.aiml24@gla.ac.in <ashishkumar12333ak@gmail.com>
2026-02-20 02:32:50 +0530

    Read and print temperature and humidity

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,31 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup(){
-    Serial.begin(9600);
-    dht.begin();
-    Serial.println("DHT11 Sensor Initialized");
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("DHT11 Sensor Initialized");
 }
 
 void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %  | ");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *");
+
+  delay(1500);
 }
+
 
 
 
